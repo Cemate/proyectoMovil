a4e41001804577fc6b8d91ca95bd4cd79fd61dda
Chelo MG <shagus.ch@gmail.com>
2018-09-30 14:24:30 -0500

    V.1.1 formato del documento listo

--- src/app/pages/principal/formato/tipos/origen-vecindad/origen-vecindad.component.ts
+++ src/app/pages/principal/formato/tipos/origen-vecindad/origen-vecindad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-origen-vecindad',
@@ -10,6 +11,14 @@ export class OrigenVecindadComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      // $("button").click(function(){
+      //     var div = $("div");
+      //     div.animate({left: '100px'}, "slow");
+      //     div.animate({fontSize: '5em'}, "slow");
+      // });
+      $(function() { $('textarea').froalaEditor(); });
+    });
   }
 
 }
